780d7a308a33dfa4c5e0598e9cbcc0faa21f68f5
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2022-12-10 20:36:38 -0800

    fixed day 2 part 1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { getTopCalorieAmount, GetTop3CalorieGroupsTotal } = require('./solutions/day-1')
-const { organizeRuckSack, getBadges } = require('./day-3')
-const { calculateScore } = require('./day-2')
+const { organizeRuckSack, getBadges } = require('./solutions/day-3')
+const { calculateScore } = require('./solutions/day-2')
 const { resolve } = require('path')
 const fs = require('fs');
 
-const input = fs.readFileSync(resolve(__dirname, './inputs/day-4.txt'), 'utf8')
+const input = fs.readFileSync(resolve(__dirname, './inputs/day-2.txt'), 'utf8')
 
-console.log()
+console.log(calculateScore(input))
--- solutions/day-2.js
+++ solutions/day-2.js
@@ -8,16 +8,20 @@ function calculateScore(strategyGuide) {
     const scores = strategyGuide.trim().split('\n').map((instruction) => {
         let [enemyMove, yourMove] = instruction.toLowerCase().trim('\r').split(' ')
         enemyMove = enemyMove.charCodeAt() - 96 // convert to numeric value
-        let score = 0;
         yourMove = moveValues[yourMove]
+        let score = yourMove
 
-        if (enemyMove < yourMove) {
+        if (
+            (enemyMove == 3 && yourMove == 1) ||
+            (enemyMove == 2 && yourMove == 3) ||
+            (enemyMove == 1 && yourMove == 2)
+            ) {
             score += 6
         } else if (enemyMove == yourMove) {
             score += 3
         }
 
-        return score + yourMove
+        return score
     })
 
     return scores.reduce((a, b) => a + b)
