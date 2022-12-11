4f5c984ebe186b4f3f2a08e1134ff40db49fc983
Weisman <weisman123@outlook.com>
2022-12-10 16:42:40 -0800

    solved part two of day 3

--- day-3.js
+++ day-3.js
@@ -17,8 +17,33 @@ function organizeRuckSack(ruckSacks) {
 
         return priority
     })
-    
+
     return priorities.reduce((a, b) => a + b)
 }
 
-module.exports = { organizeRuckSack }
+function getBadges(ruckSacks) {
+    let splitSacks = [...ruckSacks.trim().match(/(?:.+\n?){3}/g)] // Couldn't figure out the regex for splitting by by ever 3rd \n.
+    const groupedSacks = []
+
+    while (splitSacks.length > 0) {
+        groupedSacks.push(splitSacks.splice(0, 3))
+    }
+
+    const sharedItems = groupedSacks.map(sack => {
+        return [...new Set(sack[0].split('').filter(item => { return sack[1].includes(item) && sack[2].includes(item) }))]
+    })
+
+    const priorities = sharedItems.flat().map(item => {
+        let priority = item.toLowerCase().charCodeAt() - 96
+        
+        if (item !== item.toLowerCase()) { // If uppercase.
+            priority += 26
+        }
+
+        return priority
+    })
+
+    return priorities.reduce((a, b) => a + b)
+}
+
+module.exports = { organizeRuckSack, getBadges }
