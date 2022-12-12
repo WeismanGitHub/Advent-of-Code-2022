a7c5b24b66c76c8a21e2c12d676cb79eb0177ec6
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2022-12-12 06:15:08 -0800

    made partially working modelled columns

--- solutions/day-5.js
+++ solutions/day-5.js
@@ -1,23 +1,38 @@
 function getTopCrates(data) {
-    const [crates, instructions] = data.split('\n\r')
+    const [crates, instructions] = data.trimRight().split('\n\r')
     const splitCrates = crates.split('\n')
+    let modeledColumns = {}
 
-    const columns = splitCrates.slice(0, splitCrates.length - 1).map(row => {
+    splitCrates.slice(0, splitCrates.length - 1).forEach(row => {
         row = row.replaceAll('[', '').replaceAll(']', '').replace('\r', '') // Find out how to replace array of chars.
-        const column = []
 
         for (char of row) {
-            if (char !== ' ') { // Find out how to break.
-                const index = row.indexOf(char)
+            if (char !== ' ') { // Figure out how to break loop so structure is flattened.
+                let index = row.indexOf(char) + row.replaceAll(' ', '').split(char)[0].length
+                let counter = 0;
 
-                column.splice(index + index % 3, 0, char)
+                while (index >= 3) {
+                    index -= 3
+                    counter += 1
+                }
+
+                if (modeledColumns[counter]) {
+                    modeledColumns[counter].push(char)
+                } else {
+                    modeledColumns[counter] = [char]
+                }
             }
         }
-
-        return column
     })
 
-    return columns
+    return modeledColumns
+    // for (let instruction of instructions.trim().split('\n')) {
+    //     const [location, amount, destination] = instruction.match(/\d/g)
+
+    //     const crate = columns[location - 1].slice(0, amount)
+
+    //     console.log(crate)
+    // }
 }
 
 module.exports = { getTopCrates }
